9571da45551ff9019b4defc18295a13a0b41e6db
Daniel Herrera C. <daniel.herrera.castro@gmail.com>
2024-08-31 16:28:49 +0200

    Fix prev

--- movment-detection.py
+++ movment-detection.py
@@ -4,12 +4,14 @@ from tkinter import filedialog
 import cv2
 import json
 from pathlib import Path
+from line_profiler import profile
 
 logger = logging.getLogger(__name__)
 
 
+@profile
 def detect_motion(
-    video_path,
+    video_path: Path,
     movement_threshold=5000,
     min_contour_area=500,
     step_sec=3,
@@ -35,6 +37,21 @@ def detect_motion(
     ret, frame1 = cap.read()
     frame_index = 0
 
+    width = frame1.shape[0]
+    height = frame1.shape[1]
+    crop_x = int(cut_left / 100 * width if cut_left else 0)
+    crop_y = int(cut_top / 100 * height if cut_top else 0)
+    crop_xx = width - int(cut_right / 100 * width if cut_right else 0)
+    crop_yy = height - int(cut_bottom / 100 * height if cut_bottom else 0)
+
+    def frame_preprocess(frame):
+        frame = frame[crop_x:crop_xx, crop_y:crop_yy]
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        down_scale = cv2.resize(gray, (0, 0), fx=0.5, fy=0.5)
+        return down_scale
+
+    frame1 = frame_preprocess(frame1)
+
     motion_start_str = None
     motion_start = None
 
@@ -48,43 +65,12 @@ def detect_motion(
         if not ret:
             break
 
-        frame1_resized = frame1
-        frame2_resized = frame2
+        frame2 = frame_preprocess(frame2)
 
-        if cut_top:
-            frame1_resized = resize_by_cutting(frame1_resized, percentage=cut_top)
-            frame2_resized = resize_by_cutting(frame2_resized, percentage=cut_top)
-        if cut_right:
-            frame1_resized = resize_by_cutting(
-                frame1_resized, percentage=cut_right, side="right"
-            )
-            frame2_resized = resize_by_cutting(
-                frame2_resized, percentage=cut_right, side="right"
-            )
-        if cut_left:
-            frame1_resized = resize_by_cutting(
-                frame1_resized, percentage=cut_left, side="left"
-            )
-            frame2_resized = resize_by_cutting(
-                frame2_resized, percentage=cut_left, side="left"
-            )
-        if cut_bottom:
-            frame1_resized = resize_by_cutting(
-                frame1_resized, percentage=cut_bottom, side="bottom"
-            )
-            frame2_resized = resize_by_cutting(
-                frame2_resized, percentage=cut_bottom, side="bottom"
-            )
-
-        # Calculate the absolute difference between the two frames
-        gray1 = cv2.cvtColor(frame1_resized, cv2.COLOR_BGR2GRAY)
-        down_scale1 = cv2.resize(gray1, (0, 0), fx=0.5, fy=0.5)
-        gray2 = cv2.cvtColor(frame2_resized, cv2.COLOR_BGR2GRAY)
-        down_scale2 = cv2.resize(gray2, (0, 0), fx=0.5, fy=0.5)
-
-        diff = cv2.absdiff(down_scale1, down_scale2)
+        diff = cv2.absdiff(frame1, frame2)
         _, threshold_binary_image = cv2.threshold(diff, 20, 255, cv2.THRESH_BINARY)
-        dilated = cv2.dilate(threshold_binary_image, None, iterations=3)
+        # dilated = cv2.dilate(threshold_binary_image, None, iterations=3)
+        dilated = threshold_binary_image
 
         # if debug:
         #     cv2.imshow("diff", diff)
@@ -124,7 +110,7 @@ def detect_motion(
                     )  # Green contours with thickness of 2
                     # Display the image with contours
                     cv2.imshow("Contours", contour_img)
-                    cv2.imshow("down_scale1", down_scale1)
+                    cv2.imshow("down_scale1", frame1)
                     cv2.imshow("frame1", frame1)
                     cv2.waitKey(0)
                     cv2.destroyAllWindows()
@@ -135,7 +121,7 @@ def detect_motion(
                         "start": motion_start_str,
                         "end": timestamp_str,
                         "start_frames": motion_start,
-                        "end_frames": frame_count,
+                        "end_frames": frame_index,
                     }
                 )
                 motion_start_str = None  # Reset motion start
@@ -147,13 +133,13 @@ def detect_motion(
 
     # If motion was ongoing at the end of the video
     if motion_start_str is not None:
-        timestamp = frame_count / fps
+        timestamp = frame_index / fps
         motion_periods.append(
             {
                 "start": motion_start_str,
                 "end": f"{int(timestamp // 60)}:{int(timestamp % 60):02}",
                 "start_frames": motion_start,
-                "end_frames": frame_count,
+                "end_frames": frame_index,
             }
         )
 
@@ -161,52 +147,25 @@ def detect_motion(
     cap.release()
 
     # Save the motion periods to a JSON file
-    output_json = video_path.replace(".mp4", ".json")
+    output_json = video_path.with_suffix(".json")
     with open(output_json, "w") as f:
         json.dump(motion_periods, f, indent=4)
 
     print(f"Motion detection complete. Motion periods saved to {output_json}")
 
 
-def resize_by_cutting(frame, percentage=20, side="top"):
-    height, width = frame.shape[:2]
-    cut_size = percentage / 100
-
-    if side.lower() == "top":
-        # Calculate the height to cut and crop the top
-        cut_height = int(height * cut_size)
-        cropped_frame = frame[cut_height:, :]
-    elif side.lower() == "bottom":
-        # Calculate the height to cut and crop the bottom
-        cut_height = int(height * cut_size)
-        cropped_frame = frame[:-cut_height, :]
-    elif side.lower() == "left":
-        # Calculate the width to cut and crop the left
-        cut_width = int(width * cut_size)
-        cropped_frame = frame[:, cut_width:]
-    elif side.lower() == "right":
-        # Calculate the width to cut and crop the right
-        cut_width = int(width * cut_size)
-        cropped_frame = frame[:, :-cut_width]
-    else:
-        raise ValueError(
-            "Invalid side. Choose from 'top', 'bottom', 'left', or 'right'."
-        )
-
-    return cropped_frame
-
-
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
 
     # Usage
-    folder_path = Path(
-        filedialog.askdirectory(title="Select Folder Containing MP4 and JSON Files")
-    )
+    # folder_path = Path(
+    #     filedialog.askdirectory(title="Select Folder Containing MP4 and JSON Files")
+    # )
+    folder_path = Path("data")
     video_files = [f for f in folder_path.glob("*.mp4")]
 
     for file in video_files:
-        logger.info(f"Processing: {file}")
+        logger.info("Processing: %s", file)
         assert file.exists()
         detect_motion(
             file,
